c753e9f69dadd22bc64059473757d6b618105953
umar22108 <umuchtar0@gmail.com>
2025-03-25 08:42:34 +0700

    fitur download format excel

--- kito/resources/views/mitrabps/inputSurvei.blade.php
+++ kito/resources/views/mitrabps/inputSurvei.blade.php
@@ -122,11 +122,14 @@
     <!-- Modal Upload Excel -->
     <div id="uploadModal" class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 hidden">
         <div class="bg-white p-6 rounded-lg shadow-lg w-1/3">
-            <h2 class="text-xl font-bold mb-2">Unggah File Excel</h2>
-            <p class="mb-2">Tambahkan survei dengan file excel.</p>
+            <h2 class="text-xl font-bold mb-2">Import Survei</h2>
+            <p class="mb-2 text-red-700 text-sm">Pastikan format file excel yang diimport sesuai.</p>
             <form action="{{ route('upload.excelSurvei') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <input type="file" name="file" accept=".xlsx, .xls" class="border p-2 w-full">
+                    <a href="{{ asset('addSurvey.xlsx') }}" class="py-2 text-blue-500 hover:text-blue-600 text-xs">
+                        Belum punya file excel? Download template disini.
+                    </a>
                 <div class="flex justify-end mt-4">
                     <button type="button" class="px-4 py-2 bg-gray-500 text-white rounded-md mr-2" onclick="closeModal()">Batal</button>
                     <button type="submit" class="px-4 py-2 bg-orange text-white rounded-md">Unggah</button>
