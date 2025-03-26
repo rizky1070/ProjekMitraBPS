e941b75a2518e8b1c4e4b20ba2dff1cf95567c5a
umar22108 <umuchtar0@gmail.com>
2025-03-26 14:35:03 +0700

    delete report initial

--- kito/resources/views/mitrabps/reportSurvei.blade.php
+++ kito/resources/views/mitrabps/reportSurvei.blade.php
@@ -149,9 +149,6 @@
                                     <tr class="hover:bg-gray-50">
                                         <td class="px-6 py-4 whitespace-nowrap">
                                             <div class="flex items-center">
-                                                <div class="flex-shrink-0 h-10 w-10 rounded-full bg-orange-100 flex items-center justify-center">
-                                                    <span class="text-orange-600 font-medium">{{ strtoupper(substr($survei->nama_survei, 0, 1)) }}</span>
-                                                </div>
                                                 <div class="ml-4">
                                                     <div class="text-sm font-medium text-gray-900">{{ $survei->nama_survei }}</div>
                                                     <div class="text-sm text-gray-500">{{ $survei->tim }}</div>
