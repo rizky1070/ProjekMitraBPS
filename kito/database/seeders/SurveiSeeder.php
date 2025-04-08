a54d38cc5f9021f74d127b05d74241010fc840a5
rizky1070 <rizkyandwip@gmail.com>
2025-04-08 10:45:30 +0700

    menambahkan total honor

--- kito/database/seeders/SurveiSeeder.php
+++ kito/database/seeders/SurveiSeeder.php
@@ -12,25 +12,27 @@ class SurveiSeeder extends Seeder
         $surveiData = [];
 
         for ($i = 1; $i <= 20; $i++) {
-            $tahun = rand(2018, 2025); // Tahun acak antara 2018-2025
+            $tahun = rand(2018, 2025);
             $bulan = rand(1, 12);
-            $tanggal = rand(1, 28); // Membatasi maksimal 28 untuk menghindari masalah bulan
-            $honor = rand(10000, 50000); // Honor acak antara 100.000 - 500.000
-            $vol = rand(1, 10); // Volume acak antara 1-10
+            $tanggal = rand(1, 28);
+            $honor = rand(10000, 50000);
+            $vol = rand(1, 10);
+            $total_honor = $vol * $honor; // Menghitung total honor
             
             $surveiData[] = [
-                'id_provinsi' => '35', // Random antara 1 hingga 4
-                'id_kabupaten' => '16', // Random antara 1 hingga 4
-                'id_kecamatan' => rand(1, 4), // Random antara 1 hingga 4
-                'id_desa' => rand(1, 4), // Random antara 1 hingga 4
-                'nama_survei' => 'Survei ' . chr(64 + $i), // Nama survei: Survei A, Survei B, ..., Survei T
-                'lokasi_survei' => 'Lokasi ' . chr(64 + $i), // Lokasi: Lokasi A, Lokasi B, ..., Lokasi T
-                'kro' => 'Kro ' . chr(64 + $i), // KRO: Kro A, Kro B, ..., Kro T
+                'id_provinsi' => '35',
+                'id_kabupaten' => '16',
+                'id_kecamatan' => rand(1, 4),
+                'id_desa' => rand(1, 4),
+                'nama_survei' => 'Survei ' . chr(64 + $i),
+                'lokasi_survei' => 'Lokasi ' . chr(64 + $i),
+                'kro' => 'Kro ' . chr(64 + $i),
                 'jadwal_kegiatan' => $tahun . '-' . sprintf('%02d', $bulan) . '-' . sprintf('%02d', $tanggal),            
-                'status_survei' => rand(1, 3), // Status survei: 1, 2, atau 3
+                'status_survei' => rand(1, 3),
                 'tim' => 'tim ' . chr(64 + $i),
                 'honor' => $honor,
-                'vol' => $vol
+                'vol' => $vol,
+                'total_honor' => $total_honor // Menambahkan total_honor
             ];
         }
 
