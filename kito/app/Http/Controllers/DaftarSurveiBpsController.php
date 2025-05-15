718be3a0b82bbd1e6ea6c8b49feceece2e3c2cfd
umar22108 <umuchtar0@gmail.com>
2025-05-15 10:44:25 +0700

    penyesuaian input survei

--- kito/app/Http/Controllers/DaftarSurveiBpsController.php
+++ kito/app/Http/Controllers/DaftarSurveiBpsController.php
@@ -444,11 +444,9 @@ class DaftarSurveiBpsController extends Controller
     {
         $provinsi = Provinsi::where('id_provinsi', 35)->get();// Ambil semua data provinsi
         $kabupaten = Kabupaten::where('id_kabupaten', 16)->get(); // Ambil semua data kabupaten
-        $kecamatan = Kecamatan::all(); // Ambil semua data kecamatan
-        $desa = Desa::all(); // Ambil semua data desa
 
 
-        return view('mitrabps.inputSurvei', compact('provinsi', 'kabupaten', 'kecamatan', 'desa'));
+        return view('mitrabps.inputSurvei', compact('provinsi', 'kabupaten'));
     }
 
     public function getKabupaten($id_provinsi)
@@ -463,21 +461,12 @@ class DaftarSurveiBpsController extends Controller
         return response()->json($kecamatan);
     }
 
-    public function getDesa($id_kecamatan)
-    {
-        $desa = Desa::where('id_kecamatan', $id_kecamatan)->get();
-        return response()->json($desa);
-    }
-
     // Method untuk menyimpan data survei
     public function store(Request $request)
     {
         // Validasi input (hapus 'bulan_dominan' dan 'status_survei')
         $validated = $request->validate([
-            'id_kecamatan' => 'required|exists:kecamatan,id_kecamatan',
-            'id_desa' => 'required|exists:desa,id_desa',
             'nama_survei' => 'required|string|max:1024',
-            'lokasi_survei' => 'required|string|max:1024',
             'kro' => 'required|string|max:1024',
             'jadwal_kegiatan' => 'required|date',
             'jadwal_berakhir_kegiatan' => 'required|date',
@@ -523,15 +512,12 @@ class DaftarSurveiBpsController extends Controller
         $existingSurvei = Survei::where('nama_survei', $validated['nama_survei'])
             ->where('jadwal_kegiatan', $startDate->toDateString())
             ->where('jadwal_berakhir_kegiatan', $endDate->toDateString())
-            ->where('id_kecamatan', $validated['id_kecamatan'])
             ->where('bulan_dominan', $validated['bulan_dominan'])
             ->first();
 
         if ($existingSurvei) {
             // Update data yang sudah ada
             $existingSurvei->update([
-                'lokasi_survei' => $validated['lokasi_survei'],
-                'id_desa' => $validated['id_desa'],
                 'kro' => $validated['kro'],
                 'status_survei' => $validated['status_survei'],
                 'tim' => $validated['tim'],
