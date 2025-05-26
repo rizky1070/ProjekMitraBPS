a08dd42ae662ab46269c7dbc7637b2ad5bd93fb3
umar22108 <umuchtar0@gmail.com>
2025-05-26 15:20:17 +0700

    menyederhanakan style

--- kito/resources/views/setape/sekretariat/daftarLink.blade.php
+++ kito/resources/views/setape/sekretariat/daftarLink.blade.php
@@ -5,52 +5,7 @@ $title = 'Sekretariat';
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="icon" href="/Logo BPS.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/css/tom-select.css" rel="stylesheet">
-    <style>
-        .switch {
-            position: relative;
-            display: inline-block;
-            width: 50px;
-            height: 28px;
-        }
-
-        .switch input {
-            opacity: 0;
-            width: 0;
-            height: 0;
-        }
-
-        .slider {
-            position: absolute;
-            cursor: pointer;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background-color: #ccc;
-            transition: .4s;
-            border-radius: 34px;
-        }
-
-        .slider:before {
-            position: absolute;
-            content: "";
-            height: 20px;
-            width: 20px;
-            left: 4px;
-            bottom: 4px;
-            background-color: white;
-            transition: .4s;
-            border-radius: 50%;
-        }
-
-        input:checked + .slider {
-            background-color: #007BFF;
-        }
-
-        input:checked + .slider:before {
-            transform: translateX(22px);
-        }
-    </style>
+    @include('setape.switch')
 </head>
 <body class="h-full">
     <!-- SweetAlert Logic -->
