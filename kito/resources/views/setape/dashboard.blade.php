fe6b729ac560dd30cb1296b03038a86cb4862730
umar22108 <umuchtar0@gmail.com>
2025-06-02 15:49:31 +0700

    template dashboard (belum fix)

--- kito/resources/views/setape/dashboard.blade.php
+++ kito/resources/views/setape/dashboard.blade.php
@@ -31,7 +31,44 @@ $title = 'Super Tim';
         <div class="flex flex-col flex-1 overflow-hidden">
             <x-navbar></x-navbar>
             <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200 p-6">
-ini dashboard
+                <!-- Link -->
+                <div class="flex flex-col items-center justify-center bg-white rounded-lg shadow-md">
+                    <p class="text-3xl font-bold text-center my-4">
+                        Link Tersedia :
+                    </p>
+                    <div class="flex w-full justify-between">
+                        <div class="text-2xl font-bold border rounded-lg shadow-sm m-4 p-4">
+                            Link Sekretariat
+                            <ul class="block text-lg font-bold list-disc list-inside">
+                                <li>Total Link :</li>
+                                <li>Link Aktif :</li>
+                                <li>Link Nonaktif :</li>
+                            </ul>
+                        </div>
+                        <div class="text-2xl font-bold border rounded-lg shadow-sm m-4 p-4">
+                            Link Super Tim
+                            <ul class="block text-lg font-bold list-disc list-inside">
+                                <li>Total Link :</li>
+                                <li>Link Aktif :</li>
+                                <li>Link Nonaktif :</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- Kategori -->
+                <div class="flex flex-col items-center justify-center bg-white rounded-lg shadow-md mt-6">
+                    <p class="text-3xl font-bold text-center my-4">
+                        Kategori Tersedia :
+                    </p>
+                    <div class="flex w-full justify-between">
+                        <div class="text-2xl font-bold border rounded-lg shadow-sm m-4 p-4">
+                            Kategori Sekretariat :
+                        </div>
+                        <div class="text-2xl font-bold border rounded-lg shadow-sm m-4 p-4">
+                            Kategori Super Tim : 
+                        </div>
+                    </div>
+                </div>
             </main>
 
         </div>
