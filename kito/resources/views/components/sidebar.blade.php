aad3e4063719815210930b76b3fec7cb5cb354f9
rizky1070 <rizkyandwip@gmail.com>
2025-06-03 13:33:33 +0700

    sidebar dashboard

--- kito/resources/views/components/sidebar.blade.php
+++ kito/resources/views/components/sidebar.blade.php
@@ -302,7 +302,8 @@
                 request()->is('kategoripribadi') ||
                 request()->is('daftarsupertim') ||
                 request()->is('daftarsekretariat') ||
-                request()->is('daftarlinkpribadi');
+                request()->is('daftarlinkpribadi') ||
+                request()->is('setapedashboard');
 
                 @endphp
                 <div>
@@ -318,7 +319,7 @@
                 
                 <div class="dropdown-container {{ $isDropdownMitraActive ? 'block' : 'hidden' }}">
                     @if (auth()->user()->is_admin || auth()->user()->is_leader)
-                    <a class="flex items-center px-6 py-2 mt-4 ml-10 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100 {{ request()->is('daftarMitra') ? 'text-white bg-gray-700 bg-opacity-50' : '' }}"
+                    <a class="flex items-center px-6 py-2 mt-4 ml-10 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100 {{ request()->is('setapedashboard') ? 'text-white bg-gray-700 bg-opacity-50' : '' }}"
                         href="/setapedashboard">
                         <span class="mx-3">Dashboard</span>
                     </a>
