e5d061c8f38ff4742ce2718c2cfc58ff6500f8dc
rizky1070 <rizkyandwip@gmail.com>
2025-06-04 09:43:02 +0700

    responsive hal posisi mitra

--- kito/resources/views/mitrabps/crudPosisiMitra.blade.php
+++ kito/resources/views/mitrabps/crudPosisiMitra.blade.php
@@ -43,32 +43,27 @@ $title = 'Daftar Posisi Mitra';
                     <div class="overflow-x-auto">
                         <table class="min-w-full table-auto">
                             <thead>
-                                <tr class="border-b">
-                                    <th class="px-4 py-2 text-left">Nama Posisi</th>
-                                    <th class="px-4 py-2 text-left">Rate Honor</th>
-                                    <th class="px-4 py-2 text-center">Aksi</th>
+                                <tr class="bg-gray-100 border-b">
+                                    <th class="px-4 py-2 text-left text-sm font-medium">Nama Posisi</th>
+                                    <th class="px-4 py-2 text-left text-sm font-medium">Rate Honor</th>
+                                    <th class="px-4 py-2 text-center text-sm font-medium">Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach ($posisiMitra as $Posisi)
-                                <tr class="border-b">
-                                    <td class="px-4 py-2">{{ $Posisi->nama_posisi }}</td>
-                                    <td class="px-4 py-2">Rp {{ number_format($Posisi->rate_honor, 0, ',', '.') }}</td>
+                                <tr class="border-b hover:bg-gray-50">
+                                    <td class="px-4 py-2 text-sm">{{ $Posisi->nama_posisi }}</td>
+                                    <td class="px-4 py-2 text-sm">Rp {{ number_format($Posisi->rate_honor, 0, ',', '.') }}</td>
                                     <td class="px-4 py-2 text-center">
                                         <!-- Edit Button -->
-                                        <button @click="showEditModal = true; currentPosisi = {{ $Posisi->id_posisi_mitra }}; editPosisiName = '{{ $Posisi->nama_posisi }}'; editRateHonor = '{{ $Posisi->rate_honor }}'" 
-                                            class="bg-oren text-white px-2 py-1 rounded hover:bg-orange-500 mr-3">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                                                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                                            </svg>
+                                        <button @click="showEditModal = true; currentPosisi = {{ $Posisi->id_posisi_mitra }}; editPosisiName = '{{ $Posisi->nama_posisi }}'; editRateHonor = '{{ $Posisi->rate_honor }}'"
+                                            class="bg-oren text-white px-3 py-1 rounded-lg hover:bg-orange-500 mr-3">
+                                            Edit
                                         </button>
-                                        
                                         <!-- Delete Button -->
-                                        <button @click="deletePosisi({{ $Posisi->id_posisi_mitra }}, '{{ $Posisi->nama_posisi }}')" 
-                                            class="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                                                <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                                            </svg>
+                                        <button @click="deletePosisi({{ $Posisi->id_posisi_mitra }}, '{{ $Posisi->nama_posisi }}')"
+                                            class="bg-red-500 text-white px-3 py-1 rounded-lg hover:bg-red-600">
+                                            Hapus
                                         </button>
                                     </td>
                                 </tr>
